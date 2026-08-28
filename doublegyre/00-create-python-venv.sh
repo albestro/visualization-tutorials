18690bd665ba047cb7a8089d10debe720627c3f6
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2026-08-28 18:04:10 +0200

    fix for missing development files in system provided python

--- doublegyre/00-create-python-venv.sh
+++ doublegyre/00-create-python-venv.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-uv venv
+# note:
+# --managed-python ensures that there are development files
+# --python 3.12 ensures that we use the same version used by paraview (check it!)
+uv venv --managed-python --python 3.12
 uv pip install -r requirements.txt
