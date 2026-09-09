d5a5b2bb2a67b8ac30ac3f07d081bdc654caa956
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2026-09-09 11:06:21 +0200

    drop helper scripts

--- doublegyre/00-create-python-venv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# note:
-# --managed-python ensures that there are development files
-# --python 3.12 ensures that we use the same version used by paraview (check it!)
-uv venv --managed-python --python 3.12
-uv pip install -r requirements.txt
